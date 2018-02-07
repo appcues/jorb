c0292719474af5410ba3086878d208837895aacf
Andy LeClair <andyleclair@gmail.com>
2018-02-07 15:53:25 -0500

    fixing typo

--- lib/jorb/fetcher.ex
+++ lib/jorb/fetcher.ex
@@ -1,4 +1,5 @@
 defmodule Jorb.Fetcher do
+  require Logger
   use GenServer
   alias ExAws.SQS
 
@@ -8,7 +9,7 @@ defmodule Jorb.Fetcher do
 
   def poll_sqs() do
     poll_timeout = Application.get_env(:jorb, :fetching_timer) || 1000
-    Process.send_after(__MODULE__, :poll_sqs, poll_timeout)
+    Process.send_after(self(), :poll_sqs, poll_timeout)
   end
 
   def init(queue_name) do
@@ -19,11 +20,13 @@ defmodule Jorb.Fetcher do
   def handle_info(:poll_sqs, queue_name) do
     poll_sqs()
 
+
     1..Application.get_env(:jorb, :fetching_processes)
     |> Enum.each(fn(_) ->
 
       %{body: %{messages: messages}} = SQS.receive_message(queue_name, max_number_of_messages: 10)
                                        |> ExAws.request!()
+
 
       Jorb.Broker.process_batch(messages)
     end)
