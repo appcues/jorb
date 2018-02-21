05bf6c0f9bcbef4250e9283ee52ec10d060cd767
Andy LeClair <andyleclair@gmail.com>
2018-02-21 11:27:53 -0500

    updating dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jorb.Mixfile do
   def project do
     [
       app: :jorb,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
