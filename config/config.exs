0e6b7b89034eca749d9fdf575cddcc3c0779e856
Andy LeClair <andyleclair@gmail.com>
2018-02-22 15:32:50 -0500

    removing elixometer config

--- config/config.exs
+++ config/config.exs
@@ -8,15 +8,6 @@ config :jorb,
   fetching_timer: 1000,
   namespace: "Elixir.Jorb.Jobs."
 
-config(:exometer_core, report: [reporters: [{:exometer_report_tty, []}]])
-
-config(
-  :elixometer,
-  reporter: :exometer_report_tty,
-  env: Mix.env(),
-  metric_prefix: "jorb"
-)
-
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
