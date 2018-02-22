a3c9761bdbea7e03368fdcae18861f1535fa35a1
Andy LeClair <andyleclair@gmail.com>
2018-02-22 11:23:26 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Jorb.Mixfile do
   def project do
     [
       app: :jorb,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
